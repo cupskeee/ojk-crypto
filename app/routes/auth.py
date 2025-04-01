d2e2f462fede34afdfede279dcdd84597d0b52b6
cupskeee <YusufRC7@gmail.com>
2025-04-02 02:20:27 +0700

    Partialize footer, header and sidebar

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -10,6 +10,8 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
